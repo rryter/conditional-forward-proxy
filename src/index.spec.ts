9e330327066b1c20ef6e1637d99c3e81efcfd9ac
Reto Ryter <reto@twy.gmbh>
2020-04-22 02:21:57 -0500

    chore(test): added dummy test [WIP]

---
+++ src/index.spec.ts
@@ -0,0 +1,10 @@
+// tslint:disable:no-expression-statement no-object-mutation
+import test from 'ava';
+test(
+  'dummy',
+  (t, input: string, expected: string) => {
+    t.is(input, expected);
+  },
+  '1',
+  '1'
+);
